3823df1728aab718a6f3fe41edeb05f1ec0ebd24
Charlotte Dunois <github@charuru.moe>
2019-04-16 20:02:06 +0200

    Try sh

--- .travis/gen-ssl.sh
+++ .travis/gen-ssl.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
     -subj "/C=US/ST=US/L=EU/O=Travis/CN=localhost" \
     -keyout ./.travis/ssl.key  -out ./.travis/ssl.crt
